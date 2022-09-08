fa02b65882bc8b87c219ee4dd3e56d60144f1ec0
Nde-Ebom <ndekaji@gmail.com>
2022-09-08 17:39:00 +0100

    Update of script to check for email correctness
    I am making sure the email is valid before sending messages.

--- Site1-Sthyc.php
+++ Site1-Sthyc.php
@@ -4,18 +4,20 @@ $subject = 'Email de votre site web.';
 $headers = "MIME-Version: 1.0"."\r\n";
 $headers.='Content-type: text/plain; charset: utf-8'."\r\n";
 $headers.= "Message_de: ".$_POST['prenom']."\r\n";
-$message.= "Nom: ".$_POST['nom']."\r\n\r\n";
+$message= "Nom: ".$_POST['nom']."\r\n\r\n";
 $message.= "Prénom: ".$_POST['prenom']."\r\n\r\n";
 $email=filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
 if($email){
     $headers.="Repondez_à: $email";
     $message.= "Email: ".$_POST['email']."\r\n\r\n";
+    $message.= "Téléphone: ".$_POST['phone']."\r\n\r\n";
+    $message.= "Ville: ".$_POST['town']."\r\n\r\n";
+    $message.= "Pays: ".$_POST['pays']."\r\n\r\n";
+    $message.= "Message: ".$_POST['msg'];
+    $sent = mail($to,$subject,$message,$headers,'-f'.$to);
+}else{
+    echo "<h2>L'addresse email est incorrect. S'il vous plait, corriger l'addresse avant d'envoyer le message.</h2>"."\r\n";
 }
-$message.= "Téléphone: ".$_POST['phone']."\r\n\r\n";
-$message.= "Ville: ".$_POST['town']."\r\n\r\n";
-$message.= "Pays: ".$_POST['pays']."\r\n\r\n";
-$message.= "Message: ".$_POST['msg'];
-$sent = mail($to,$subject,$message,$headers,'-f'.$to);
 ?>
 <html>
     <head>
@@ -26,7 +28,7 @@ $sent = mail($to,$subject,$message,$headers,'-f'.$to);
         <?php if(isset($sent) && $sent){?>
             <h1><?php echo "$name, Sthyc vous remercie pour votre collaboration.";?></h1>
             <?php }else{?>
-                <h1><?php echo "Erreur: Message non envoyer. Veuillez ressayer.";?></h1>
+                <h1><?php echo "Erreur: Message non envoyer. Veuillez reessayer.";?></h1>
             <?php }?>
             <a href="contact.htm">Rentrez sur le site</a>
     </body>
